ddecd8509b1a32d038e36641fd44b8052378a555
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-23 20:44:54 +0330

    add ModeToggle in header

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -13,27 +13,30 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { useLocation } from "react-router";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 interface Props {
   children: ReactNode;
 }
 
 export default function Layout({ children }: Props) {
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
 
   const renderHeader = (
-    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-      <div className="flex items-center gap-2 px-4">
+    <header className="flex justify-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 px-4">
+      <div className="flex items-center gap-2">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem>
-              <BreadcrumbPage>{pathname?.split('/')[1]}</BreadcrumbPage>
+              <BreadcrumbPage>{pathname?.split("/")[1]}</BreadcrumbPage>
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
       </div>
+
+      <ModeToggle />
     </header>
   );
 
