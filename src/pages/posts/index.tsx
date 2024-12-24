ec8361db9e7db1fff088a3a5ea28762ad94726f4
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-24 22:14:02 +0330

    add label

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -6,6 +6,7 @@ import { PostType } from "./types";
 
 // components
 import PostCard from "./post-card";
+import { Checkbox } from "@/components/ui/checkbox";
 
 const Posts = () => {
   const { isPending, error, data } = useFetchData<PostType[]>(["posts"]);
@@ -15,10 +16,13 @@ const Posts = () => {
   if (error) return "An error has occurred: " + error.message;
 
   return (
-    <div className="grid gap-3 grid-cols-1 lg:grid-cols-2">
-      {data?.map((post) => (
-        <PostCard key={post.id} post={post} />
-      ))}
+    <div className="space-y-4">
+      <Checkbox />
+      <div className="grid gap-3 grid-cols-1 lg:grid-cols-2">
+        {data?.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
     </div>
   );
 };
