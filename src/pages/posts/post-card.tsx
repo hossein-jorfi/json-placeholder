295d21db9c7cd0b1852a75caf5be478302782fcc
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-24 21:28:25 +0330

    post card styles

--- src/pages/posts/post-card.tsx
+++ src/pages/posts/post-card.tsx
@@ -19,9 +19,16 @@ const PostCard = ({ post }: Props) => {
   };
 
   return (
-    <div key={post.id} className="border p-4 rounded-lg flex flex-col gap-3 justify-between">
+    <div
+      key={post.id}
+      className="border p-4 rounded-lg flex flex-col gap-3 justify-between"
+    >
+      <div className="flex justify-between">
+        <p className="font-semibold text-sm"># {post.id}</p>
+        like
+      </div>
       <p className="font-bold text-2xl">{post.title}</p>
-      <p>{post.body}</p>
+      <p className="text-muted-foreground">{post.body}</p>
       <Button className="w-fit" onClick={() => navigateHandler(post?.id)}>
         See More
       </Button>
