a591874afb59e3b82839a2572b85f0970ebf9d8d
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-25 12:27:00 +0330

    pagination component

--- src/pages/posts/posts-pagination.tsx
+++ src/pages/posts/posts-pagination.tsx
@@ -8,27 +8,75 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 
-import { PaginationReturnType } from "@/hooks/use-pagination";
+import { usePagination } from "@/hooks/use-pagination";
 
 interface Props {
-  paginationHook: PaginationReturnType;
+  onPageChange: (n: number) => void;
+  totalCount: number;
+  siblingCount: number;
+  currentPage: number;
+  pageSize: number;
 }
 
-const PostsPagination = ({ paginationHook }: Props) => {
-  console.log(paginationHook)
+const PostsPagination = (props: Props) => {
+  const {
+    onPageChange,
+    totalCount,
+    siblingCount = 1,
+    currentPage,
+    pageSize,
+  } = props;
+
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  });
+
+  // If there are less than 2 times in pagination range we shall not render the component
+  if (currentPage === 0 || (paginationRange?.length || 0) < 2) {
+    return null;
+  }
+
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  const lastPage = paginationRange?.[paginationRange.length - 1];
+
   return (
     <Pagination>
       <PaginationContent>
-        <PaginationItem onClick={() => paginationHook.onChangePage('', 2)}>
+        <PaginationItem onClick={onPrevious}>
           <PaginationPrevious />
         </PaginationItem>
-        <PaginationItem>
+        {paginationRange?.map((pageNumber) => {
+          // If the pageItem is a DOT, render the DOTS unicode character
+          if (pageNumber === 'DOTS') {
+            return <li className="pagination-item dots">&#8230;</li>;
+          }
+
+          // Render our Page Pills
+          return (
+            <li
+              onClick={() => onPageChange(+pageNumber)}
+            >
+              {pageNumber}
+            </li>
+          );
+        })}
+        {/* <PaginationItem>
           <PaginationLink>1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
+        </PaginationItem> */}
+        {/* <PaginationItem>
           <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem>
+        </PaginationItem> */}
+        <PaginationItem onClick={onNext}>
           <PaginationNext />
         </PaginationItem>
       </PaginationContent>
