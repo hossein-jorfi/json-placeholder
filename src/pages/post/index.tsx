35e7fae3c204124ac32bd434e1a39a63381fc251
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-26 16:12:33 +0330

    hadnle like for single post page

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -6,9 +6,13 @@ import { useParams } from "react-router";
 import PostUser from "./post-user";
 import PostComments from "./post-comments";
 import PostSkeleton from "./post-skeleton";
+import { useEffect, useState } from "react";
+import { handlePostLikeLocalStorage, isPostLiked } from "@/lib/post-utils";
 
 const Post = () => {
   const { postId } = useParams();
+
+  const [liked, setLiked] = useState(false);
 
   const { isPending, error, data } = useFetchData<PostType>([
     "posts",
@@ -24,18 +28,26 @@ const Post = () => {
     !isPending
   );
 
+  useEffect(() => {
+    setLiked(isPostLiked(postId ? +postId : 0));
+  }, [liked, postId]);
+
+  const onLikeHandler = () => {
+    handlePostLikeLocalStorage(postId ? +postId : 0);
+    setLiked(!liked);
+  };
+
   if (error) return "An error has occurred: " + error.message;
 
-  if (isPending) return <PostSkeleton />
+  if (isPending) return <PostSkeleton />;
 
   return (
     <div className="flex justify-center items-center sm:mt-10 xl:mt-20">
       <div className="border-2 rounded-lg w-full md:w-2/3 p-5 space-y-3">
         <div className="flex items-center justify-between">
           <PostUser data={userQuery.data} isLoading={userQuery.isPending} />
-          <Button variant="ghost" size="icon">
-            {/* {liked === true ? <Heart color="red" fill="red" /> : <Heart />} */}
-            <Heart />
+          <Button variant="ghost" size="icon" onClick={onLikeHandler}>
+            {liked === true ? <Heart color="red" fill="red" /> : <Heart />}
           </Button>
         </div>
         <div className="w-full border" />
