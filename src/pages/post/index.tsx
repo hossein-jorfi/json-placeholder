94ff9916b24c613d9e04044e75fb7af74a50de4a
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-26 12:47:25 +0330

    like button

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,5 +1,7 @@
 import useFetchData from "@/service/use-fetch-data";
 import { PostType } from "../posts/types";
+import { Button } from "@/components/ui/button";
+import { Heart } from "lucide-react";
 
 const Post = () => {
   const { error, data } = useFetchData<PostType>(["posts", "1"]);
@@ -10,6 +12,13 @@ const Post = () => {
   return (
     <div className="flex justify-center items-center mt-10 md:mt-20">
       <div className="border-2 rounded-lg w-full md:w-2/3 p-5">
+        <div className="flex items-center justify-between">
+          <p># {data?.id}</p>
+          <Button variant="ghost" size="icon">
+            {/* {liked === true ? <Heart color="red" fill="red" /> : <Heart />} */}
+            <Heart />
+          </Button>
+        </div>
         <div className="space-y-3">
           <h3 className=" font-bold text-3xl">{data?.title}</h3>
           <h3 className="text-2xl text-muted-foreground">{data?.body}</h3>
