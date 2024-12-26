921e5d9cd33cbbf83528a8620bf778ababe30e9a
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-26 15:22:13 +0330

    Comments

--- src/pages/post/post-comments.tsx
+++ src/pages/post/post-comments.tsx
@@ -1,4 +1,6 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { CommentType } from "@/defenitions";
+import { cn } from "@/lib/utils";
 
 interface Props {
   data: CommentType[] | undefined;
@@ -7,13 +9,19 @@ interface Props {
 
 const PostComments = ({ data }: Props) => {
   return (
-    <div className="flex flex-col">
-      {data?.map((comment) => (
-        <div className="flex flex-col">
-          <p>{comment.name}</p>
-          <p>{comment.body}</p>
-        </div>
-      ))}
+    <div className="flex flex-col max-h-72 overflow-hidden gap-3">
+      Comments
+      <ScrollArea className="flex flex-col">
+        {data?.map((comment, index) => (
+          <div
+            key={comment.id}
+            className={cn("flex flex-col border p-3 rounded-lg my-3", index == 0 && 'mt-0')}
+          >
+            <p className="font-semibold opacity-70">{comment.name}</p>
+            <p className="opacity-50">{comment.body}</p>
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
