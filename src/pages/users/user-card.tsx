6e6b46619c91d07ec3b5950cc443d07e1e2e2e22
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-28 21:30:45 +0330

    InfoItem

--- src/pages/users/user-card.tsx
+++ src/pages/users/user-card.tsx
@@ -1,8 +1,9 @@
+import { ReactNode } from "react";
 import { UserType } from "@/defenitions";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { MapPin } from "lucide-react";
+import { MapPin, PhoneCall } from "lucide-react";
 
-const UserCard = ({ name, email, address }: UserType) => {
+const UserCard = ({ name, email, address, phone }: UserType) => {
   return (
     <div className="border p-4 rounded-lg flex flex-col gap-3 justify-between">
       <div className="flex items-center gap-2">
@@ -19,14 +20,21 @@ const UserCard = ({ name, email, address }: UserType) => {
         </div>
       </div>
 
-      <div className="text-sm font-semibold mt-1 text-foreground/80">
-        <div className="flex gap-1 items-center">
-          <MapPin  className="w-4"/>
-          <p>{address.city}</p>
-        </div>
+      <div className="text-sm font-semibold mt-1 text-foreground/80 flex flex-col gap-1">
+        <InfoItem icon={<MapPin className="w-4" />} info={address.city} />
+        <InfoItem icon={<PhoneCall className="w-4" />} info={phone} />
       </div>
     </div>
   );
 };
 
 export default UserCard;
+
+const InfoItem = ({ icon, info }: { info: string; icon: ReactNode }) => {
+  return (
+    <div className="flex gap-1 items-center">
+      {icon}
+      <p>{info}</p>
+    </div>
+  );
+};
