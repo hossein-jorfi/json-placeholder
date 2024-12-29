fb0a53a18058b422120f68151d75e103cc246478
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-29 11:35:35 +0330

    user query

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,9 +1,21 @@
+import { UserType } from "@/defenitions";
+import useFetchData from "@/service/use-fetch-data";
 import { useParams } from "react-router";
 
 const User = () => {
   const { userId } = useParams();
 
-  return <div>User # {userId}</div>;
+  const userQuery = useFetchData<UserType>(["users", userId || ""]);
+
+  if (userQuery.isPending) return "Loading...";
+
+  return (
+    <div className="flex justify-center items-center sm:mt-10 xl:mt-20">
+      <div className="border-2 rounded-lg w-full md:w-2/3 p-5 space-y-3">
+        {userQuery.data?.name}
+      </div>
+    </div>
+  );
 };
 
 export default User;
