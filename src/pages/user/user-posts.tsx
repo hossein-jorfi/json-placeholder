91c078f660681bb967e802550f3e26684d36aa4b
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-29 14:23:31 +0330

    navigateHandler

--- src/pages/user/user-posts.tsx
+++ src/pages/user/user-posts.tsx
@@ -2,12 +2,19 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { PostType } from "@/defenitions";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
 
 interface Props {
   data: PostType[] | undefined;
 }
 
 const UserPosts = ({ data }: Props) => {
+  const navigate = useNavigate();
+
+  const navigateHandler = (id: number) => {
+    navigate(`/posts/detail/${id}`);
+  };
+
   return (
     <div className="flex flex-col max-h-96 overflow-hidden gap-2">
       <p className="text-sm font-semibold">Posts</p>
@@ -21,10 +28,12 @@ const UserPosts = ({ data }: Props) => {
             )}
           >
             <div className="flex gap-2 items-center">
-              <p className="font-semibold opacity-70">{post.title}</p>
+              <p className="font-semibold opacity-90">{post.title}</p>
             </div>
-            <p className="opacity-50">{post.body}</p>
-            <Button className="h-8">See More</Button>
+            <p className="opacity-60">{post.body}</p>
+            <Button className="h-8" onClick={() => navigateHandler(post.id)}>
+              See More
+            </Button>
           </div>
         ))}
       </ScrollArea>
