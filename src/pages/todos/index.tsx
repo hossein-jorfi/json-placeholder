dc492edde3e6b74861c4130dc1eff30a721655d2
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-30 12:45:48 +0330

    get todos

--- src/pages/todos/index.tsx
+++ src/pages/todos/index.tsx
@@ -1,9 +1,12 @@
+import { TodoType } from "@/defenitions";
+import useFetchData from "@/service/use-fetch-data";
+
 const Todos = () => {
-      return (
-            <div>
-                  Todos
-            </div>
-      );
+  const { isPending, error, data } = useFetchData<TodoType[]>(["todos"]);
+
+  if (isPending) return "Loading...";
+
+  return <div>Todos</div>;
 };
 
 export default Todos;
