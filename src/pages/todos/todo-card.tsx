5f106bb625aca07749cca5ea2dca9e145da980ba
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-30 13:59:25 +0330

    tooltip fir todo status

--- src/pages/todos/todo-card.tsx
+++ src/pages/todos/todo-card.tsx
@@ -1,6 +1,12 @@
 import { TodoType, UserType } from "@/defenitions";
 import UserAvatar from "./user-avatar";
 import { CircleCheck, CircleX } from "lucide-react";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 interface Props {
   todo: TodoType;
@@ -12,13 +18,22 @@ const TodoCard = ({ todo, user }: Props) => {
     <div className="border-2 p-3 rounded-lg min-h-44 flex flex-col justify-between">
       <div className="flex justify-between">
         <p className="text-sm font-bold text-primary/70"># {todo.id}</p>
-        <p>
-          {todo.completed ? (
-            <CircleCheck className="text-green-600 w-6 h-6" />
-          ) : (
-            <CircleX className="text-warning w-6 h-6" />
-          )}
-        </p>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div>
+                {todo.completed ? (
+                  <CircleCheck className="text-green-600 w-6 h-6" />
+                ) : (
+                  <CircleX className="text-warning w-6 h-6" />
+                )}
+              </div>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{todo.completed ? "Done" : "In Progress"}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
       <p className="font-semibold text-primary/70">{todo.title}</p>
       <UserAvatar data={user} />
