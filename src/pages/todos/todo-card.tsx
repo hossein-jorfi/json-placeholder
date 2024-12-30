9cf4dd522113b2e925139e114f5e13f77a442fc1
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-12-30 13:54:56 +0330

    todo status

--- src/pages/todos/todo-card.tsx
+++ src/pages/todos/todo-card.tsx
@@ -1,5 +1,6 @@
 import { TodoType, UserType } from "@/defenitions";
 import UserAvatar from "./user-avatar";
+import { CircleCheck, CircleX } from "lucide-react";
 
 interface Props {
   todo: TodoType;
@@ -11,7 +12,13 @@ const TodoCard = ({ todo, user }: Props) => {
     <div className="border-2 p-3 rounded-lg min-h-44 flex flex-col justify-between">
       <div className="flex justify-between">
         <p className="text-sm font-bold text-primary/70"># {todo.id}</p>
-        <p>{todo.completed ? "Done" : "In Progress"}</p>
+        <p>
+          {todo.completed ? (
+            <CircleCheck className="text-green-600 w-6 h-6" />
+          ) : (
+            <CircleX className="text-warning w-6 h-6" />
+          )}
+        </p>
       </div>
       <p className="font-semibold text-primary/70">{todo.title}</p>
       <UserAvatar data={user} />
