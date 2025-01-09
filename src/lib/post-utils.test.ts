7166b18c7ef0088e0f81cd8413a6236a5ceed54f
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-09 10:33:23 +0330

    vitest

--- src/lib/post-utils.test.ts
+++ src/lib/post-utils.test.ts
@@ -1,12 +1,10 @@
 // sum.test.js
-import { expect, test, it } from "vitest";
+import { expect, test } from "vitest";
 
 function sum(a: number, b: number) {
   return a + b;
 }
 
 test("adds 1 + 2 to equal 3", () => {
-//   it("Sum Logic", () => {
     expect(sum(1, 2)).toBe(3);
-//   });
 });
