15d09000d468583c07b570f45a0f75ed0584049e
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-09 10:53:19 +0330

    snapshot

--- src/lib/post-utils.test.ts
+++ src/lib/post-utils.test.ts
@@ -6,5 +6,6 @@ function sum(a: number, b: number) {
 }
 
 test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
+    const restult = sum(1, 2)
+    expect(restult).toMatchSnapshot()
 });
