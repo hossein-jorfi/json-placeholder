80a13b2d1c90873bc7660687dbcbf45b61d7ea3f
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-09 15:17:43 +0330

    test push given id to liked-post array

--- src/lib/post-utils.test.ts
+++ src/lib/post-utils.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { handlePostLikeLocalStorage, STORAGE_KEY } from "./post-utils";
-import { afterEach, beforeEach } from "node:test";
+import { afterEach, beforeEach } from "vitest";
 
 describe("Like Posts", () => {
   afterEach(() => {
@@ -13,6 +13,13 @@ describe("Like Posts", () => {
   test("if STORAGE_KEY not exist in localstorage create it first", () => {
     handlePostLikeLocalStorage(1);
     const result = localStorage.getItem(STORAGE_KEY);
+    expect(result).toBeTruthy();
+  });
+
+  test("push given id to liked-post array", () => {
+    const POST_ID = 1
+    handlePostLikeLocalStorage(POST_ID);
+    const result = JSON.parse(localStorage.getItem(STORAGE_KEY) || "");
     expect(result).toBeTruthy();
   });
 });
