663c0d6c3ced87742e59ad5414f9e49e070af521
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-09 14:44:10 +0330

    storageName variable

--- src/lib/post-utils.ts
+++ src/lib/post-utils.ts
@@ -1,25 +1,27 @@
+const storageName = "liked-post"
+
 export const handlePostLikeLocalStorage = (postId: number) => {
-  if (!localStorage?.getItem("liked-post")) {
-    localStorage.setItem("liked-post", JSON.stringify([]));
+  if (!localStorage?.getItem(storageName)) {
+    localStorage.setItem(storageName, JSON.stringify([]));
   }
-  const likedPosts = JSON?.parse(localStorage?.getItem("liked-post") || "");
+  const likedPosts = JSON?.parse(localStorage?.getItem(storageName) || "");
 
   const isLiked = likedPosts.find((id: number) => id == postId);
   if (isLiked) {
     localStorage.setItem(
-      "liked-post",
+      storageName,
       JSON.stringify(likedPosts?.filter((id: number) => id != postId))
     );
   } else {
-    localStorage.setItem("liked-post", JSON.stringify([...likedPosts, postId]));
+    localStorage.setItem(storageName, JSON.stringify([...likedPosts, postId]));
   }
 };
 
 export const isPostLiked = (postId: number) => {
-  if (!localStorage?.getItem("liked-post")) {
+  if (!localStorage?.getItem(storageName)) {
     return false;
   } else {
-    const likedPosts = JSON?.parse(localStorage?.getItem("liked-post") || "");
+    const likedPosts = JSON?.parse(localStorage?.getItem(storageName) || "");
     if (likedPosts?.find((i: number) => i == postId) == undefined) {
       return false;
     } else {
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    reporters: ['html', 'basic']
+    // reporters: ['html', 'basic'],
   },
 });
